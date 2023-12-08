d1c69e220d7d02a1cd72026fad42eb8a099fbeb6
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-12-08 14:11:25 -0700

    remove http from schemas, ran swagger

--- swagger.ts
+++ swagger.ts
@@ -5,18 +5,18 @@ const doc = {
     title: 'Recipe API',
     description: 'Recipe API for Team Project ',
   },
-  servers: [
-    {
-        url: "http://localhost:8080/",
-        description: "Local server"
-    },
-    {
-        url: "https://recipesapi-2dkf.onrender.com",
-        description: "Live server"
-    }
-  ],
-  // host: 'recipesapi-2dkf.onrender.com',
-  // schemes: ['https, http']
+  // servers: [
+  //   {
+  //       url: "http://localhost:8080/",
+  //       description: "Local server"
+  //   },
+  //   {
+  //       url: "https://recipesapi-2dkf.onrender.com",
+  //       description: "Live server"
+  //   }
+  // ],
+  host: 'recipesapi-2dkf.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
