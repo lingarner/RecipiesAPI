60d3ad284c5fd2defdc51ce4a2bbf8b710098e4f
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-12-08 12:02:00 -0700

    deleteSingle is working

--- controllers/baseController.ts
+++ controllers/baseController.ts
@@ -45,25 +45,31 @@ export async function getAll(req: Request, res: Response): Promise<void> {
     }
   }
 
-export async function deleteSingle(req: Request, res: Response): Promise<void> {
+  
+  export async function deleteSingle(req: Request, res: Response): Promise<void> {
     try {
-        if (!ObjectId.isValid(req.params.id)) {
-            res.status(400).json('Must use a valid id to delete a recipe.');
-            return;
-        }
-
-        const id = new ObjectId(req.params.id);
-        const result = await mongodb.getDb().collection(req.baseUrl.substring(1)).deleteOne({ _id: id });
-
-        if (result.deletedCount === 1) {
-            res.status(200).json({ message: 'Recipe deleted successfully.' });
-        } else {
-            throw new Error('Recipe not found.');
-        }
+      if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json({ error: 'Must use a valid id to delete a recipe.' });
+        return;
+      }
+  
+      const id = new ObjectId(req.params.id);
+      const collectionName = req.baseUrl.substring(1);
+      //@ts-ignore
+      const collection = mongodb.getDb().collection(collectionName);
+  
+      const result = await collection.deleteOne({ _id: id });
+  
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: 'Recipe deleted successfully.' });
+      } else {
+        throw new Error('Recipe not found.');
+      }
     } catch (error: any) {
-        res.status(500).json(error.message || 'Something went wrong');
+      res.status(500).json({ error: error.message || 'Something went wrong' });
     }
-}
+  }
+  
 
 export async function addSingle(req: Request, res: Response): Promise<void> {
     try {
