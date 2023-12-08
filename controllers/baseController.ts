7058d8b885ec8c2103d773cb2e0ebc26982e762b
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-12-08 12:11:06 -0700

    all routes work

--- controllers/baseController.ts
+++ controllers/baseController.ts
@@ -72,53 +72,61 @@ export async function getAll(req: Request, res: Response): Promise<void> {
   
 
 export async function addSingle(req: Request, res: Response): Promise<void> {
-    try {
-        const recipe = {
-            ingredients: req.body.ingredients,
-            recipeTitle: req.body.recipeTitle,
-            prepTimeorbakingTime: req.body.prepTimeorbakingTime,
-            comments: req.body.comments,
-            instructions: req.body.instructions,
-            utensils: req.body.utensils
-        };
+try {
+    const recipe = {
+    ingredients: req.body.ingredients,
+    recipeTitle: req.body.recipeTitle,
+    prepTimeorbakingTime: req.body.prepTimeorbakingTime,
+    comments: req.body.comments,
+    instructions: req.body.instructions,
+    utensils: req.body.utensils,
+    };
 
-        const result = await mongodb.getDb().collection(req.baseUrl.substring(1)).insertOne(recipe);
+    const collectionName = req.baseUrl.substring(1);
+    //@ts-ignore
+    const collection = mongodb.getDb().collection(collectionName);
 
-        if (result.acknowledged) {
-            res.status(200).json({ message: 'Recipe added successfully.' });
-        } else {
-            throw new Error('Failed to add recipe.');
-        }
-    } catch (error: any) {
-        res.status(500).json(error.message || 'Something went wrong');
+    const result = await collection.insertOne(recipe);
+
+    if (result.acknowledged) {
+    res.status(200).json({ message: 'Recipe added successfully.' });
+    } else {
+    throw new Error('Failed to add recipe.');
     }
+} catch (error: any) {
+    res.status(500).json({ error: error.message || 'Something went wrong' });
+}
 }
 
 export async function editSingle(req: Request, res: Response): Promise<void> {
     try {
-        if (!ObjectId.isValid(req.params.id)) {
-            res.status(400).json('Must use a valid id to edit a recipe.');
-            return;
-        }
-
-        const recipe = {
-            ingredients: req.body.ingredients,
-            recipeTitle: req.body.recipeTitle,
-            prepTimeorbakingTime: req.body.prepTimeorbakingTime,
-            comments: req.body.comments,
-            instructions: req.body.instructions,
-            utensils: req.body.utensils
-        };
-
-        const id = new ObjectId(req.params.id);
-        const result = await mongodb.getDb().collection(req.baseUrl.substring(1)).replaceOne({ _id: id }, recipe);
-
-        if (result.modifiedCount === 1) {
-            res.status(200).json({ message: 'Recipe edited successfully.' });
-        } else {
-            throw new Error('Recipe not found.');
-        }
+      if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json({ error: 'Must use a valid id to edit a recipe.' });
+        return;
+      }
+  
+      const recipe = {
+        ingredients: req.body.ingredients,
+        recipeTitle: req.body.recipeTitle,
+        prepTimeorbakingTime: req.body.prepTimeorbakingTime,
+        comments: req.body.comments,
+        instructions: req.body.instructions,
+        utensils: req.body.utensils,
+      };
+  
+      const id = new ObjectId(req.params.id);
+      const collectionName = req.baseUrl.substring(1);
+      //@ts-ignore
+      const collection = mongodb.getDb().collection(collectionName);
+  
+      const result = await collection.replaceOne({ _id: id }, recipe);
+  
+      if (result.modifiedCount === 1) {
+        res.status(200).json({ message: 'Recipe edited successfully.' });
+      } else {
+        throw new Error('Recipe not found.');
+      }
     } catch (error: any) {
-        res.status(500).json(error.message || 'Something went wrong');
+      res.status(500).json({ error: error.message || 'Something went wrong' });
     }
-}
+  }
