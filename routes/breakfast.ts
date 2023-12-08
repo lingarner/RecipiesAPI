b981ff2d39575965bd14de565e64dd8c80d16acd
Nathan Gartside <gar18014@byui.edu>
2023-12-08 16:25:05 -0700

    Added to breakfeast

--- routes/breakfast.ts
+++ routes/breakfast.ts
@@ -2,13 +2,14 @@
 
 import express, { Router } from 'express';
 import * as baseController from '../controllers/baseController';
+import * as validator from '../utilities/validation';
 
 const router: Router = express.Router();
 
 router.get('/:id', baseController.getSingle);
 router.get('/', baseController.getAll);
-router.post('/', baseController.addSingle);
-router.put('/:id', baseController.editSingle);
+router.post('/', validator.newRecipe, validator.checkValidation, baseController.addSingle);
+router.put('/:id', validator.newRecipe, validator.checkValidation, baseController.editSingle);
 router.delete('/:id', baseController.deleteSingle);
 
 export default router;
