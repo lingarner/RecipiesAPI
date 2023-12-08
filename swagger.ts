1be14a89fc704237e3d1d1fd6144f3d47f1b0f96
Lindsay Garner <gar21034@byui.edu>
2023-12-07 21:05:31 -0700

    changed server to host

--- swagger.ts
+++ swagger.ts
@@ -1,22 +1,22 @@
-import swaggerAutogen from 'swagger-autogen';
+const swaggerAutogen = require('swagger-autogen');
 
 const doc = {
   info: {
     title: 'Recipe API',
     description: 'Recipe API for Team Project ',
   },
-  servers: [
-    {
-        url: "http://localhost:8080/",
-        description: "Local server"
-    },
-    {
-        url: "https://recipesapi-2dkf.onrender.com",
-        description: "Live server"
-    }
-  ],
-  // host: 'http://localhost:8080',
-  // schemes: ['http']
+  // servers: [
+  //   {
+  //       url: "http://localhost:8080/",
+  //       description: "Local server"
+  //   },
+  //   {
+  //       url: "https://recipesapi-2dkf.onrender.com",
+  //       description: "Live server"
+  //   }
+  // ],
+  host: 'http://localhost:8080',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
